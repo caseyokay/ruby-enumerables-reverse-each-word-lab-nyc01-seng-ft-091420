361f42900bb0e01ede23f545a21c4665d772ae05
Casey B <caseyoksana@gmail.com>
2020-09-10 00:00:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 #    def reverse_each_word(sentence)
 #  sentence.reverse.split.reverse.join(" ")
 # end
-def reverse_each_word(string)
-  string.split.collect {|word| word.reverse}.join(" ")
-end
+# def reverse_each_word(string)
+#   string.split.collect {|word| word.reverse}.join(" ")
+# end
 
